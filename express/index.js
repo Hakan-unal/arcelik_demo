e5e0a00d7c3a3018351768b2df41226530452172
Con Sınov <hakanunal@devtagon.com>
2022-04-01 12:38:29 +0300

    update

--- express/index.js
+++ express/index.js
@@ -10,22 +10,22 @@ const app = express();
 
 app.use(express.json())
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: "text/*" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors({ origin: true, credentials: false }));
 
-
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
 
 
 app.post("/authenticate", (req, res) => {
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Origin', '*');
 
 
     const { code } = req.body;
