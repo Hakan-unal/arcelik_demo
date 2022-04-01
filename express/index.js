108a71ab9552bc654d18fc7dcefcf071b9f4b577
Con Sınov <hakanunal@devtagon.com>
2022-04-01 12:42:18 +0300

    test

--- express/index.js
+++ express/index.js
@@ -15,12 +15,13 @@ app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: "text/*" }));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors({ origin: true, credentials: false }));
+app.use(cors({ origin: true }));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+
     next();
 });
 
