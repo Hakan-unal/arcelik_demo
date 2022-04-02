080ea628c45ff64dc3476c03eda697eb53e23641
Con Sınov <hakanunal@devtagon.com>
2022-04-02 20:45:35 +0300

    udpate

--- express/index.js
+++ express/index.js
@@ -39,11 +39,9 @@ app.get("/test", (req, res) => {
     res.json({ message: "Hello world" });
 })
 
-app.get("/", (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.json({ message: "Hello world" });
-})
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '../build/index.html'));
+});
 
 app.post("/test", (req, res) => {
     res.setHeader('Content-Type', 'text/html');
