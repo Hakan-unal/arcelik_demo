7db9c932b19d868b2d37a5364fb5967a0375d7c7
Con Sınov <hakanunal@devtagon.com>
2022-04-02 22:24:19 +0300

    updated

--- src/pages/landingPage/LandingPageContent.js
+++ src/pages/landingPage/LandingPageContent.js
@@ -7,13 +7,15 @@ const { id, redirect } = require("../../config.json");
 
 const LandingPageContent = (props) => {
   const [isFirstPage, setIsFirstPage] = useState(true)
-  useEffect = () => {
+
+  useEffect(() => {
     const parsedUrl = new URL(window.location.href)
     const code = parsedUrl.searchParams.get("code")
     if (code !== null) {
       setIsFirstPage(false)
     }
-  }
+  }, [])
+
   return (<div>
     {isFirstPage ?
       <a href={`https://github.com/login/oauth/authorize?scope=user&client_id=${id}&redirect_uri=${redirect}`}><AiFillGithub size={100} color="black" /></a>
