7dc94a50e0e0cd4b3d01bc31bce20f6c0f4986d9
Con Sınov <hakanunal@devtagon.com>
2022-04-02 13:03:59 +0300

    local worked

--- express/index.js
+++ express/index.js
@@ -19,15 +19,14 @@ app.use(cors({ origin: true }));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
+
     next();
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: "text/*" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({ origin: true }));
 
 app.get("/test", (req, res) => {
     res.setHeader('Content-Type', 'text/html');
